56fdff328a74d5c53051269849aee6d6a625e3e3
Duncan McGregor <duncan@oneeyedmen.com>
2013-06-29 23:56:01 +0100

    See how things work with junit-quickcheck - not bad, but need to stabilize random inputs

--- src/test/java/org/hamcrest/approvals/QuickCheckTest.java
+++ src/test/java/org/hamcrest/approvals/QuickCheckTest.java
@@ -1,6 +1,7 @@
 package org.hamcrest.approvals;
 
 import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
 import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -10,17 +11,24 @@ import org.junit.runner.RunWith;
 @RunWith(org.junit.contrib.theories.Theories.class)
 public class QuickCheckTest {
 
+    /*
+      This seems like a good idea, but junit-quickcheck generators aren't stable
+     */
+
     @ClassRule public static final TheoryApprovalsRule theoryRule = new TheoryApprovalsRule("src/test/java");
     @Rule public final TheoryApprovalsRule.TheoryApprover approver = theoryRule.approver();
 
     @org.junit.contrib.theories.Theory
-    public void legacyMethod_output(@ForAll String s, @ForAll int i) {
-        approver.lockDown(legacyMethod(s, i), s, i);
+    public void legacyMethod_output(@ForAll(sampleSize = 3) Strings s, @ForAll(sampleSize = 3) @InRange(minInt = 0, maxInt = 2) int i, @ForAll(sampleSize = 2) boolean b) {
+        approver.lockDown(legacyMethod(s.name(), i, b), s.name(), i, b);
     }
 
 
-    public String legacyMethod(String s, int i) {
-        return s + i;
+    public String legacyMethod(String s, int i, boolean b) {
+        return s + i + b;
     }
 
+    public static enum Strings {
+        apple, banana, kumquat
+    }
 }
