cb00f86b020a417341f9a965f63baa74ef733b0d
Duncan McGregor <duncan@oneeyedmen.com>
2013-07-11 12:19:49 +0100

    Add MacFileSystemSourceOfApproval

---
+++ src/main/java/org/rococoa/okeydoke/MacFileSystemSourceOfApproval.java
@@ -0,0 +1,27 @@
+package org.rococoa.okeydoke;
+
+import java.io.File;
+
+public class MacFileSystemSourceOfApproval extends FileSystemSourceOfApproval {
+    
+    public MacFileSystemSourceOfApproval(File directory) {
+        super(directory);
+    }
+
+    public MacFileSystemSourceOfApproval(File srcRoot, Package packege, File actualDir) {
+        super(srcRoot, packege, actualDir);
+    }
+
+    public MacFileSystemSourceOfApproval(File approvedDir, File actualDir) {
+        super(approvedDir, actualDir);
+    }
+
+    public MacFileSystemSourceOfApproval(File root, Package packege) {
+        super(root, packege);
+    }
+
+    @Override
+    public void reportFailure(String testname, Throwable e) {
+        System.err.format("To see differences...\nopendiff '%s' '%s'\n", actualFileFor(testname), approvedFileFor(testname));
+    }
+}
