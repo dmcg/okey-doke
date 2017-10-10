d8109af2c0a563ad16e4e271bbeb72be91e0d5c2
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-10 11:43:25 +0100

    Add Name override for test name

--- src/main/java/com/oneeyedmen/okeydoke/junit/StandardTestNamer.java
+++ src/main/java/com/oneeyedmen/okeydoke/junit/StandardTestNamer.java
@@ -3,6 +3,8 @@ package com.oneeyedmen.okeydoke.junit;
 import com.oneeyedmen.okeydoke.Name;
 import org.junit.runner.Description;
 
+import java.lang.reflect.Method;
+
 public class StandardTestNamer implements TestNamer {
 
     @Override
@@ -11,8 +13,31 @@ public class StandardTestNamer implements TestNamer {
     }
 
     private String nameFromMethod(Description description) {
+        String override = nameFromMethodAnnotation(description);
+        if (override != null)
+            return "." + override;
+        String methodNameFromDescription = description.getMethodName();
+        return methodNameFromDescription == null ? "" : "." + methodNameFromDescription;
+    }
+
+    private String nameFromMethodAnnotation(Description description) {
+        Method method = methodFrom(description);
+        if (method == null)
+            return null;
+        Name annotation = method.getAnnotation(Name.class);
+        return annotation == null ? null : annotation.value();
+    }
+
+    private Method methodFrom(Description description) {
+        Class<?> testClass = description.getTestClass();
         String methodName = description.getMethodName();
-        return methodName == null ? "" : "." + methodName;
+        if (testClass == null || methodName == null)
+            return null;
+        try {
+            return testClass.getMethod(methodName);
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
     }
 
     private String nameFromClass(Description description) {
--- src/test/java/com/oneeyedmen/okeydoke/junit/TestNameAnnotationTest.java
+++ src/test/java/com/oneeyedmen/okeydoke/junit/TestNameAnnotationTest.java
@@ -10,6 +10,7 @@ public class TestNameAnnotationTest {
     @Rule public final ApprovalsRule approver = ApprovalsRule.usualRule();
 
     @Test
+    @Name("my test")
     public void test(){
         approver.assertApproved("banana");
     }
