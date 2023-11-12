8bc6ab777c010c28752ad867791ea7cb4053c1f3
Duncan McGregor <duncan@oneeyedmen.com>
2023-11-12 17:32:39 +0100

    Update readme to prioritise JUnit5

--- create_readme.sh
+++ create_readme.sh
@@ -9,7 +9,7 @@ function write_file_contents {
 
 echo "
 [![Download](https://maven-badges.herokuapp.com/maven-central/com.oneeyedmen/okeydoke/badge.svg?style=flat-square
-)](https://search.maven.org/artifact/com.oneeyedmen/okeydoke)
+)](https://search.maven.org/artifact/com.oneemvnyedmen/okeydoke)
 
 okey-doke
 =========
@@ -18,43 +18,34 @@ An Approval Testing library for Java and JUnit - like [Llewellyn Falco's](http:/
 
 A [helping hand](http://youtu.be/EbqaxWjIgOg) for many testing problems.
 
-## JUnit 4
-
-The basic mode of operation is
-[ApprovalsRuleTest](src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsRuleTest.java)
-to compare current thing with an approved version and fail with a diff if they aren't the same.
-"  > README.md
-
-write_file_contents src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsRuleTest.java >> README.md
-
-echo "
-Next try [LockDownTest](src/test/java/com/oneeyedmen/okeydoke/examples/LockDownTest.java)
-for easy testing of legacy code.
-"  >> README.md
-
-write_file_contents src/test/java/com/oneeyedmen/okeydoke/examples/LockDownTest.java >> README.md
-
-echo "
-Then move on to
-- [TranscriptTest](src/test/java/com/oneeyedmen/okeydoke/examples/TranscriptTest.java)
-for producing approved files with interactions
-- [Konsent](https://github.com/dmcg/konsent) for producing Gerkin-like output from your tests that the customer can approve.
-
 ## JUnit 5
 
-Thanks to @jshiell we now have basic JUnit 5 support.
-
-The basic mode of operation is
+Add an `@ExtendWith(ApprovalsExtension.class)` to your test class to get access to an approver in every test method
 
 [ApprovalsExtensionTest](src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsExtensionTest.java)
 to compare current thing with an approved version and fail with a diff if they aren't the same.
-"  >> README.md
+"  > README.md
 
 write_file_contents src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsExtensionTest.java >> README.md
 
 echo "
+If your source is in src/test/kotlin use [KotlinApprovalsExtension] instead.
 
 ## IntelliJ
 
-Oh, and I should mention, there is an [IntelliJ plugin](https://github.com/s4nchez/okey-doke-idea) (thanks @s4nchez) to help approve your output.
+There is an [IntelliJ plugin](https://github.com/s4nchez/okey-doke-idea) (thanks @s4nchez) to help approve your output.
+
+## JUnit 4
+
+We still support JUnit 4 with a Rule -
+[ApprovalsRuleTest](src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsRuleTest.java)
+to compare current thing with an approved version and fail with a diff if they aren't the same.
+"  >> README.md
+
+write_file_contents src/test/java/com/oneeyedmen/okeydoke/examples/ApprovalsRuleTest.java >> README.md
+
+
+echo "
+
+
 " >> README.md
